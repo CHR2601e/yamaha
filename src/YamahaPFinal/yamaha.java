ea534a76a654255d596b504e03617801d74f3313
USUARIO <USUARIO@LAPTOP-GF27DIQH.lan>
2024-05-30 23:09:44 -0500

    Tabajo final de estructura de datos

---
+++ src/YamahaPFinal/yamaha.java
@@ -0,0 +1,11 @@
+
+package YamahaPFinal;
+public class yamaha {
+
+    public static void main(String[] args) {
+       login ventana1 = new login();
+       
+       ventana1.setVisible(true);
+    }
+    
+}
